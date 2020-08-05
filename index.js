f83af0bbd4c6eeb124acd971315faf529c326386
MuhammadZubair210 <mzubairabdullah210@gmail.com>
2020-08-05 23:38:45 +0500

    .

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 1994;
+const db = require("./queries");
+var cors = require("cors");
+// app.use(cors());
+
+app.use( (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+  next();
+});
+
+
+app.options("*", cors());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express and Postgres API" });
+});
+
+app.post("/companyusers", db.getAllUsers);
+// app.get('/vehicle/:id',db.getVehicleDisplayById)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
---
+++ queries.js
@@ -0,0 +1,68 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "me", //db user
+  host: "127.0.0.1", //db host etc: 127.0.0.1
+  database: "energygigs", //db name
+  password: "password", // password
+  port: 5432, // db port etc: 5432 for postgresql
+});
+// `INSERT INTO companyusers (companyname, email, password, companyaddress, city, zipcode, companysize, industry, companywebsite, logo) VALUES(${obj.details.companyname}, ${obj.details.companyEmailAddress}, ${obj.details.password}, ${obj.comapnyAddress}, ${obj.city}, ${obj.zip}, ${obj.conpanySize}, "null", "/photo-1514870262631-55de0332faf6?", "55de0332faf6?");`,
+
+const getAllUsers = (request, response) => {
+  let obj = request.body.data;
+  pool.query(
+    "INSERT INTO companyusers (companyname, email, password, companyaddress, city, zipcode, companysize, industry, companywebsite, logo) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);",
+    [
+      obj.details.companyname,
+      obj.details.companyEmailAddress,
+      obj.details.password,
+      obj.comapnyAddress,
+      obj.city,
+      obj.zip,
+      obj.conpanySize,
+      "null",
+      obj.companyWebsite,
+      obj.uploadLogo,
+    ],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      //   console.log("results.row", results.rows);
+      response.status(200).json(results.rows);
+    }
+  );
+  console.log(request.body);
+//   response.status(200).json("response.body")
+
+//   next();
+};
+
+// const getVehicleDisplayById = (request, response) => {
+//     const id = parseInt(request.params.id)
+
+// pool.query('select * from users where vehicle_id = $1', [id], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//     })
+// }
+
+// const getVehicleDisplayById = (request, response) => {
+//     const id = parseInt(request.params.id)
+
+//     pool.query('select * from vehicles where vehicle_id = $1', [id], (error, results) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//     })
+// }
+
+module.exports = {
+  // getAllVehicleDisplay,
+  // getVehicleDisplayById,
+  getAllUsers,
+};
